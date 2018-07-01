1a6a1775f39e96ff82199796a3ad142ff463c7ae
mikladoka <40727455+mikladoka@users.noreply.github.com>
2018-07-01 11:40:50 -0700

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/u
 $(lsb_release -cs) \
 stable"
 sudo apt-get update
-sudo apt-get install docker-c
+sudo apt-get install docker-ce -y
 
 #install docker compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
